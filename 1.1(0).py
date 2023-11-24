dbd4c4c3c7053b0d36b821a20a56710142751336
vigglv <144350072+vigglv@users.noreply.github.com>
2023-11-25 08:06:50 +0900

    Add files via upload

---
+++ 1.1(0).py
@@ -0,0 +1,10 @@
+def f(a, b):
+    return a + b
+i = input('Два числа: ')
+a, b = map(int, i.split())
+if -10 ** 9 <= a <= 10 ** 9 and -10 ** 9 <= b <= 10 ** 9:
+    r = f(a, b)
+    print('Ответ: ', r)
+else:
+    print('Не удовлетворяет условию')
+
